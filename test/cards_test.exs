d4809d65b25443f57c99daf84b5fd12741d14d42
João Marcelo Oliveira de Souza <joaomarcelo9393@gmail.com>
2020-04-12 17:15:49 -0300

    Test create_deck and contains?

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,11 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck creates 20 cards" do
+    assert Cards.create_deck() |> length() == 20
+  end
+
+  test "contains?" do
+    assert Cards.create_deck() |> Cards.contains?("Ace of Spades") == true
   end
 end
