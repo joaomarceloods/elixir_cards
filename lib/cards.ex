94e16009f455a0aad0ec03b1ab8521451f001445
João Marcelo Oliveira de Souza <joaomarcelo9393@gmail.com>
2020-04-12 11:34:09 -0300

    Cards.shuffle

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,11 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def create_deck do
+    ["Ace", "Two", "Three"]
   end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
 end
